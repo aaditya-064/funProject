fd3fc571f4500c6cf18eff6d8bbf3427f14f095d
unknown <aadityastha123456@gmail.com>
2024-12-19 09:58:45 +0545

    changed nothing

--- yesScript.js
+++ yesScript.js
@@ -1,6 +1,6 @@
 const dateButton = document.querySelector(".button_");
 dateButton.addEventListener("click", () => {
   prompt(
-    "ENTER THE DATE (I OBVIOUSLY WON'T GET THE ANSWER CUZ I DON'T KNOW BACKEND)"
+    "ENTER THE DATE [OFC, I WON'T GET THE DATA, TOO BORED TO DO THE BACKEND :) ]"
   );
 });
